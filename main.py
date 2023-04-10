e64ebf7310dd00b810ece8d09ee74a90636cff96
EarlHikky <andrew.malofeev@gmail.com>
2023-04-10 15:50:39 +0300

    fetch_nasa_images() added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
+import re
 from pathlib import Path
 from os.path import splitext
+from os.path import split as splittitle
 from pprint import pprint
 from urllib.parse import urlparse
+from environs import Env
 
 
 def get_extension(url):
@@ -13,13 +16,12 @@ def get_extension(url):
 def save_image(url, path, image_title):
     response = requests.get(url)
     extension = get_extension(url)
-    Path(path).mkdir(parents=True, exist_ok=True)
     with open(f'{path}/{image_title}{extension}', 'wb') as file:
         file.write(response.content)
 
 
 def fetch_spacex_last_launch():
-    url = "https://api.spacexdata.com/v5/launches/"
+    url = 'https://api.spacexdata.com/v5/launches/'
     response = requests.get(url)
     response.raise_for_status()
     launches = response.json()
@@ -35,6 +37,23 @@ def fetch_spacex_last_launch():
             return
 
 
-# fetch_spacex_last_launch()
-# fetch_nasa_20_images()
+def fetch_nasa_images():
+    url = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': env('NASA_API_KEY'), 'count': 10}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    images = response.json()
+    for image in images:
+        if image['media_type'] == 'image':
+            url = image['hdurl'] if image['hdurl'] else image['url']
+            path = './images/nasa'
+            Path(path).mkdir(parents=True, exist_ok=True)
+            image_title = re.sub(r'\W', '', image['title'])
+            save_image(url, path, image_title)
 
+
+env = Env()
+env.read_env()
+
+fetch_spacex_last_launch()
+fetch_nasa_images()

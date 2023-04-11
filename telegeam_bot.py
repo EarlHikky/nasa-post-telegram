146e3ff8bc749110ba854b4df2fe3617f55d209b
EarlHikky <101412518+EarlHikky@users.noreply.github.com>
2023-04-11 11:52:55 +0300

    publish photo to Telegram channel.

--- telegeam_bot.py
+++ telegeam_bot.py
@@ -1,6 +1,7 @@
 import argparse
 import telegram
 from environs import Env
+from telegram import InputMediaDocument
 
 env = Env()
 env.read_env()
@@ -13,4 +14,9 @@ chat_id = parser.parse_args().telegram_chat_id
 telegram_chat_id = parser.parse_args().telegram_chat_id
 bot = telegram.Bot(token=env('TELEGRM_BOT_API_TOKEN'))
 
-bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+# bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+with open('./images/spacex/157/spacex1.jpg', 'rb') as mediafile:
+    image = InputMediaDocument(media=mediafile)
+    bot.send_media_group(chat_id=chat_id, media=[image])
+
+# image = InputMediaDocument(media=open('./images/spacex/157/spacex1.jpg', 'rb'))

3ae5b73156dd34e7e7adb6369526e5132ae15bf7
EarlHikky <andrew.malofeev@gmail.com>
2023-04-11 16:07:49 +0300

    добавлено ручное отправление файла

--- space_pictures_telegram_bot.py
+++ space_pictures_telegram_bot.py
@@ -13,16 +13,23 @@ def main():
     parser = argparse.ArgumentParser(description='Space_Pictures_Telegram_Bot')
     parser.add_argument("telegram_chat_id", help="id чата в Telegram")
     parser.add_argument("--period", help="период отправки изображения (в часах)", default=4)
+    parser.add_argument("--file", help="отправит конкретный файл", default='')
     chat_id = parser.parse_args().telegram_chat_id
     send_period = int(parser.parse_args().period) * 3600
-    while True:
-        for root, dirs, files in os.walk('./images'):
-            if files:
-                random.shuffle(files)
-                for file in files:
-                    with open(f'{os.path.join(root, file)}', 'rb') as mediafile:
-                        bot.send_photo(chat_id=chat_id, photo=mediafile)
-                    time.sleep(send_period)
+    path_file = parser.parse_args().file
+    if not path_file:
+        while True:
+            for root, dirs, files in os.walk('./images'):
+                if files:
+                    random.shuffle(files)
+                    for file in files:
+                        with open(f'{os.path.join(root, file)}', 'rb') as mediafile:
+                            bot.send_photo(chat_id=chat_id, photo=mediafile)
+                        time.sleep(send_period)
+    else:
+        with open(path_file, 'rb') as mediafile:
+            bot.send_photo(chat_id=chat_id, photo=mediafile)
+            return None
 
 
 if __name__ == '__main__':

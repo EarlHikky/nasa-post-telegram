0c05d23b16e2ba06307fc1d9f87aa808af24dc0d
EarlHikky <andrew.malofeev@gmail.com>
2023-04-11 15:17:18 +0300

    добавлена отправка по расписанию

---
+++ space_pictures_telegram_bot.py
@@ -0,0 +1,29 @@
+import argparse
+import random
+import time
+import os
+import telegram
+from environs import Env
+
+
+def main():
+    env = Env()
+    env.read_env()
+    bot = telegram.Bot(token=env('TELEGRM_BOT_API_TOKEN'))
+    parser = argparse.ArgumentParser(description='Space_Pictures_Telegram_Bot')
+    parser.add_argument("telegram_chat_id", help="id чата в Telegram")
+    parser.add_argument("--period", help="период отправки изображения (в часах)", default=4)
+    chat_id = parser.parse_args().telegram_chat_id
+    send_period = int(parser.parse_args().period) * 3600
+    while True:
+        for root, dirs, files in os.walk('./images'):
+            if files:
+                random.shuffle(files)
+                for file in files:
+                    with open(f'{os.path.join(root, file)}', 'rb') as mediafile:
+                        bot.send_photo(chat_id=chat_id, photo=mediafile)
+                    time.sleep(send_period)
+
+
+if __name__ == '__main__':
+    main()
